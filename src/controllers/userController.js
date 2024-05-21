a5d13a6fe4e120a185160d02db93049cc2e838fc
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2024-05-20 22:19:23 -0300

    feat: created reset and forgot-password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,8 @@ const pool = require("../config/database");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
 
 exports.register = (req, res) => {
   const {
@@ -75,4 +77,103 @@ exports.login = (req, res, next) => {
       return res.json({ message: "Logged in successfully!", token: token });
     });
   })(req, res, next);
+};
+
+exports.forgotPassword = (req, res) => {
+  const { email } = req.body;
+  if (!email) return res.status(400).send("Email é obrigatório.");
+
+  pool.query(
+    "SELECT * FROM usuarios WHERE email = ?",
+    [email],
+    (err, results) => {
+      if (err) throw err;
+      if (!results[0]) return res.status(404).send("Email não encontrado.");
+
+      const token = crypto.randomBytes(20).toString("hex");
+      const expireTime = new Date(Date.now() + 3600000);
+
+      pool.query(
+        "UPDATE usuarios SET reset_password_token = ?, reset_password_expires = ? WHERE email = ?",
+        [token, expireTime, email],
+        (err, results) => {
+          if (err) throw err;
+
+          const transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            auth: {
+              user: process.env.SMTP_USER,
+              pass: process.env.SMTP_PASS,
+            },
+          });
+
+          transporter.verify((error, success) => {
+            if (error) {
+              console.error(
+                "Erro ao verificar a conexão com o servidor de email:",
+                error
+              );
+              return res
+                .status(500)
+                .send("Erro ao verificar a conexão com o servidor de email.");
+            } else {
+              console.log(
+                "Servidor de email está pronto para enviar mensagens"
+              );
+            }
+          });
+
+          const mailOptions = {
+            to: email,
+            from: process.env.SMTP_USER,
+            subject: "Password Reset",
+            text:
+              `Você está recebendo este e-mail porque você (ou alguém) solicitou a redefinição da senha para sua conta.\n\n` +
+              `Por favor, clique no seguinte link ou cole-o no seu navegador para completar o processo dentro de uma hora da recepção deste e-mail:\n\n` +
+              `http://${req.headers.host}/reset/${token}\n\n` +
+              `Se você não solicitou isso, por favor ignore este e-mail e sua senha permanecerá inalterada.\n`,
+          };
+
+          transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+              console.error("Erro ao enviar o e-mail:", err);
+              return res.status(500).send("Erro ao enviar o e-mail.");
+            }
+            console.log("Email enviado:", info.response);
+            res.send("Email enviado com sucesso!");
+          });
+        }
+      );
+    }
+  );
+};
+
+exports.resetPassword = (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+
+  if (!password) return res.status(400).send("Senha é obrigatória.");
+
+  pool.query(
+    "SELECT * FROM usuarios WHERE reset_password_token = ? AND reset_password_expires > NOW()",
+    [token],
+    (err, results) => {
+      if (err) throw err;
+      if (!results[0])
+        return res.status(400).send("Token inválido ou expirado.");
+
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(password, salt);
+
+      pool.query(
+        "UPDATE usuarios SET password = ?, reset_password_token = NULL, reset_password_expires = NULL WHERE id = ?",
+        [hash, results[0].id],
+        (err) => {
+          if (err) throw err;
+          res.send("Senha redefinida com sucesso!");
+        }
+      );
+    }
+  );
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,6 +5,8 @@ const auth = require("../middleware/auth");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
+router.post("/forgot-password", userController.forgotPassword);
+router.post("/reset/:token", userController.resetPassword);
 router.get("/protected", auth, (req, res) => {
   res.json({ message: "This is a protected route", userId: req.userId });
 });
